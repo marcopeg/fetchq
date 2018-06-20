722d401abf63c093dde2e7983258f3799a4a55a0
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-06-20 15:28:12 +0200

    #8 Add run all maintenance

--- pg-extension/tests/_run.sql
+++ pg-extension/tests/_run.sql
@@ -1,16 +1,18 @@
 
-SELECT * FROM fetchq_test__init();
-SELECT * FROM fetchq_test__create_queue();
-SELECT * FROM fetchq_test__drop_queue();
-SELECT * FROM fetchq_test__metrics();
-SELECT * FROM fetchq_test__push_01();
-SELECT * FROM fetchq_test__push_02();
-SELECT * FROM fetchq_test__push_03();
-SELECT * FROM fetchq_test__pick_01();
-SELECT * FROM fetchq_test__pick_02();
-SELECT * FROM fetchq_test__pick_03();
-SELECT * FROM fetchq_test__pick_04();
-SELECT * FROM fetchq_test__mnt_make_pending_01();
-SELECT * FROM fetchq_test__mnt_reschedule_orphanse_01();
-SELECT * FROM fetchq_test__mnt_mark_dead_01();
+-- SELECT * FROM fetchq_test__init();
+-- SELECT * FROM fetchq_test__create_queue();
+-- SELECT * FROM fetchq_test__drop_queue();
+-- SELECT * FROM fetchq_test__metrics();
+-- SELECT * FROM fetchq_test__push_01();
+-- SELECT * FROM fetchq_test__push_02();
+-- SELECT * FROM fetchq_test__push_03();
+-- SELECT * FROM fetchq_test__pick_01();
+-- SELECT * FROM fetchq_test__pick_02();
+-- SELECT * FROM fetchq_test__pick_03();
+-- SELECT * FROM fetchq_test__pick_04();
+-- SELECT * FROM fetchq_test__mnt_make_pending_01();
+-- SELECT * FROM fetchq_test__mnt_reschedule_orphanse_01();
+-- SELECT * FROM fetchq_test__mnt_mark_dead_01();
+-- SELECT * FROM fetchq_test__mnt_run_01();
+SELECT * FROM fetchq_test__mnt_run_all_01();
 
